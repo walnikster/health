598ce3e892ef8c7f3141a4226ca9721d3dedb0ec
niki <niki@theliferocks.com>
2018-05-20 18:29:36 +0200

    styling enhanced

--- src/app/components/weight-form/weight-form.component.ts
+++ src/app/components/weight-form/weight-form.component.ts
@@ -1,50 +1,47 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FlashMessagesService } from 'angular2-flash-messages';
-import { WeightService } from '../../services/weight.service';
-import { Router } from '@angular/router';
+import { Component, OnInit, ViewChild } from "@angular/core"
+import { FlashMessagesService } from "angular2-flash-messages"
+import { WeightService } from "../../services/weight.service"
+import { Router } from "@angular/router"
 
-import { Weight } from '../../models/Weight'; 
+import { Weight } from "../../models/Weight"
 @Component({
-  selector: 'app-weight-form',
-  templateUrl: './weight-form.component.html',
-  styleUrls: ['./weight-form.component.css']
+  selector: "app-weight-form",
+  templateUrl: "./weight-form.component.html",
+  styleUrls: ["./weight-form.component.css"]
 })
 export class WeightFormComponent implements OnInit {
-
   weight: Weight = {
-    date: '',
-    weight: 0,
+    date: "",
+    weight: 0
   }
 
-
-  @ViewChild('clientForm') form: any;
+  @ViewChild("clientForm") form: any
 
   constructor(
     private flashMessage: FlashMessagesService,
     private weightService: WeightService,
     private router: Router
-  ) { }
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  onSubmit({value, valid}: {value: Weight, valid: boolean}) {
-
-    if(!valid) {
+  onSubmit({ value, valid }: { value: Weight; valid: boolean }) {
+    if (!valid) {
       // Show error
-      this.flashMessage.show('Please fill out the form correctly', {
-        cssClass: 'alert-danger', timeout: 4000
-      });
+      this.flashMessage.show("Please fill out the form correctly", {
+        cssClass: "alert-danger",
+        timeout: 4000
+      })
     } else {
       // Add new client
-      this.weightService.newWeight(value);
+      this.weightService.newWeight(value)
       // Show message
-      this.flashMessage.show('New weight added', {
-        cssClass: 'alert-success', timeout: 4000
-      });
+      this.flashMessage.show("New weight added", {
+        cssClass: "alert-success",
+        timeout: 4000
+      })
       // Redirect to dash
-      this.router.navigate(['/weight']);
+      this.router.navigate(["/weight"])
     }
   }
-
-} 
+}
