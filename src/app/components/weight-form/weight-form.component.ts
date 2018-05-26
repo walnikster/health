28a6162a5ff681c6de6ae0b9640a4433220097a2
niki <niki@theliferocks.com>
2018-05-26 20:43:14 +0200

    update angular version and other dependecies

--- src/app/components/weight-form/weight-form.component.ts
+++ src/app/components/weight-form/weight-form.component.ts
@@ -27,20 +27,16 @@ export class WeightFormComponent implements OnInit {
 
   onSubmit({ value, valid }: { value: Weight; valid: boolean }) {
     if (!valid) {
-      // Show error
       this.flashMessage.show("Please fill out the form correctly", {
         cssClass: "alert-danger",
         timeout: 4000
       })
     } else {
-      // Add new client
       this.weightService.newWeight(value)
-      // Show message
       this.flashMessage.show("New weight added", {
         cssClass: "alert-success",
         timeout: 4000
       })
-      // Redirect to dash
       this.router.navigate(["/weight"])
     }
   }
